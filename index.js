664b3f7a0a8114b7897b809f0e6779841e6c06dd
Suraj Yadav <56670036+Suraj8108@users.noreply.github.com>
2023-09-15 16:22:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
-var http = require("http");
 
-http.createServer(function (request, response) {
-   // Send the HTTP header 
-   // HTTP Status: 200 : OK
-   // Content Type: text/plain
-   response.writeHead(200, {'Content-Type': 'text/plain'});
-   
-   // Send the response body as "Hello World"
-   response.end('8B65F5773F961FE8AC00D986D1B9D6173E1BEDE41E39DB2CE872222209B089ED.7bf0e83074a62a1.comodoca.com');
-}).listen(80);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:80/');
+var express = require('express');
+var app = express();
+app.get('/.well-known/pki-validation/A96F7BD2590C506AFE33979046C346CD.txt', function (req, res) {
+  res.send('8B65F5773F961FE8AC00D986D1B9D6173E1BEDE41E39DB2CE872222209B089ED.7bf0e83074a62a1.comodoca.com');
+});
+app.listen(80, function () {
+  console.log('Example app listening on port 80!');
+});
